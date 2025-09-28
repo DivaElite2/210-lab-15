7b12c2a349d7c5dea638e601c5b1faaa8a48dd27
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-09-28 12:44:00 -0700

    Class setup

--- movie.cpp
+++ movie.cpp
@@ -5,25 +5,32 @@
 using namespace std;
 
 class Movie {
-    private:
+private:
     int releaseYear;
     string title;
-    string screenWriter;
+    string screenWriterName;
 
-    public:
+public:
     int getReleaseYear()  {return releaseYear;}
     void setReleaseYear(int releaseYear)  {this -> releaseYear; }
 
     string getTitle()  {return title;}
     void setTitle(string title)  {this -> title;}
 
-    string getScreenWriter(string screen)  {}
-
+    string getScreenWriter()  {return screenWriterName; }
+    void setScreenWriterName(string getScreenWriterName) {this ->screenWriterName;}
+// member print function
+ void print() {
+ cout << "Title: " <<endl;
+ cout << "Release Year: " <<endl;
+ cout << "Screen writer name: " <<endl;
+ }
 
 };
 
 int main()
 {
+ cout<< fixed << setp
 
     return 0;
 }
